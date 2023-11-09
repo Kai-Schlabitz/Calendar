9b1088c374eedef450eafe04b3bd7ce6ec163c79
Kai Schlabitz <schlabitz@uni-potsdam.de>
2023-11-09 13:03:33 +0100

    refactor printing, seperate main function into smaller functions

--- calendar.py
+++ calendar.py
@@ -1,31 +1,8 @@
 """This Program allows user to enter a year and a month and display its repective calender.
 The Program is based on a concept called Zeller's Congruence """
+# https://en.wikipedia.org/wiki/Zeller%27s_congruence
 
 import sys
-
-
-def print1(i, a, b):  # Defining Function for 1st print statement
-    if i == 1:
-        print(" " * a, i, end="  ")
-    elif 1 < i < 10:
-        print(" ", i, end=" ")
-    if i == b:
-        print("\n", end="")
-
-
-def print2(i, a, b, c):  # Defining Function for 2nd print statement
-    if i == 1:
-        print(" " * a, i, end="  ")
-    elif 1 < i < 10:
-        print(" ", i, end=" ")
-    if i == b or i == c:
-        print("\n", end="")
-
-
-def print3(i, a, b, c):  # Defining Function for 3rd print statement
-    print("", i, end=" ")
-    if i == a or i == b or i == c:
-        print("\n", end="")
 
 
 def get_user_input():
@@ -45,25 +22,55 @@ def get_user_input():
         print("Please only enter a number between 1 and 12 for the month.")
         sys.exit()
 
+    return input_year, input_month
+
+
+def get_weekday_of_first_day_of_month(year, month):
     # In the concept of Zeller congruence,
     # January is counted as the 11th month of the last year,
     # February is the 12th month of the last year,
     # and March is the 1st month of the current year.
-    if input_month == 1:
-        return input_year - 1, 11
+    if month == 1:
+        year -= 1
+        month = 11
+    elif month == 2:
+        year -= 1
+        month = 12
+    else:
+        month -= 2
 
-    if input_month == 2:
-        return input_year - 1, 12
+    years_per_century = 100
+    leap_year_period = 4
 
-    return input_year, input_month - 2
+    # day of month
+    q = 1
 
-def main():
-    year, month = get_user_input()
+    # Calculations below are based on zeller congruence
+    # https://en.wikipedia.org/wiki/Zeller%27s_congruence
+    # year of century
+    K = year % years_per_century
 
+    # zero based century
+    J = year // years_per_century
+    # The zero based century of a year just chops off the last two digits.
+    # For example it is 20 for the year 2000 but 19 for 1999.
+
+    
+    leap_year_offset = K // leap_year_period
+    weekday_progression_offset = J // leap_year_period - 2 * J
+
+    # this term adjusts for the variation in the days of the month
+    M = (13 * month - 1) // 5
+
+    weekday = (q + M + K + leap_year_offset + weekday_progression_offset) % 7
+
+    return weekday
+
+
+def get_days_in_month(year, month):
     # adjust the number of days in february for leap years
     year_is_leap_year = year % 4 == 0
-    
-    # A dict for the number of days present in a month
+
     month_day_dict = {
         1: 31,
         2: 28 + int(year_is_leap_year),
@@ -78,86 +85,38 @@ def main():
         11: 30,
         12: 31
     }
-
-    days = month_day_dict[month]  # the number days present in the chosen month
-
-    years_per_century = 100
-    leap_year_period = 4
     
-    # https://en.wikipedia.org/wiki/Zeller%27s_congruence
-    # Calculations below are based on zeller congruence
-    m = (13 * month - 1) // 5
-    year_of_century = year % years_per_century
-    zero_based_century = year // years_per_century
-    leap_year_offset = year_of_century // leap_year_period
-    weekday_progression_offset = zero_based_century // leap_year_period - 2 * zero_based_century
+    days = month_day_dict[month]
+    return days
 
-    monday, tuesday, wednesday, thursday, friday, saturday, sunday = (
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
+
+def print_calendar(first_day_weekday, days_in_month):
+    days_in_week = 7
+    first_row_days = days_in_week - first_day_weekday
+    last_row_overhang = (days_in_month - first_row_days) % days_in_week
+
+    calendar_boxes = (
+        [' '] * first_day_weekday
+        + list(range(1, days_in_month + 1))
+        + [' '] * last_row_overhang
     )
 
-    weekdays = (sunday, monday, tuesday, wednesday, thursday, friday, saturday)
+    weekday_labels = ("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat")
+    print(*weekday_labels, sep='\t')  # printing Header of the calender
+    
+    for row in range(0, len(calendar_boxes), days_in_week):
+        for box in calendar_boxes[row : row + days_in_week]:
+            print(f"{box:>3}", end='\t')
+        print()
 
-    equation = (1 + m + year_of_century + leap_year_offset + weekday_progression_offset) % 7
-    weekdays[equation].append(1)
+def main():
+    year, month = get_user_input()
+    
+    first_day_weekday = get_weekday_of_first_day_of_month(year, month)
 
-    print("Sun Mon Tue Wed Thu Fri Sat")  # printing Header of the calender
-    # Printing The Calender!!!!!!!!!!
-    for i in range(1, 10):
-        if 1 in sunday:
-            print("", i, end="  ")
-            if i == 7:
-                print("\n", end="")
+    days_in_month = get_days_in_month(year, month)
 
-        elif 1 in monday:
-            print1(i, 4, 6)
-
-        elif 1 in tuesday:
-            print1(i, 8, 5)
-
-        elif 1 in wednesday:
-            print1(i, 12, 4)
-
-        elif 1 in thursday:
-            print1(i, 16, 3)
-
-        elif 1 in friday:
-            print2(i, 20, 2, 9)
-
-        elif 1 in saturday:
-            print2(i, 24, 1, 8)
-
-    for i in range(10, days + 1):
-        if 1 in sunday:
-            print("", i, end=" ")
-            if i in (7, 14, 21, 28):
-                print("\n", end="")
-
-        elif 1 in monday:
-            print3(i, 13, 20, 27)
-
-        elif 1 in tuesday:
-            print3(i, 12, 19, 26)
-
-        elif 1 in wednesday:
-            print3(i, 11, 18, 25)
-
-        elif 1 in thursday:
-            print3(i, 10, 17, 24)
-
-        elif 1 in friday:
-            print3(i, 16, 23, 30)
-
-        elif 1 in saturday:
-            print3(i, 15, 12, 29)
-
+    print_calendar(first_day_weekday, days_in_month)
 
 if __name__ == "__main__":
     main()
